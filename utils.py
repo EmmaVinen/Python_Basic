2b6f946ee81b3bbb852ca0dad72983b724ed79e0
Emma <emma.vinen@hotmail.co.uk>
2024-02-28 19:15:31 +0000

    added docs to logger function

--- utils.py
+++ utils.py
@@ -1,8 +1,20 @@
 import logging
 
 
-
 def get_logger(name, level=logging.INFO):
+    """
+    This function returns a logger object that can be used to 
+    log messages to the console
+    it sets some basic formatting and defaults to a logging level of INFO
+
+    Args:
+        name (_type_): ideally this should be the name of the module/file that
+        is calling this function
+        level (_type_, optional): _description_. Defaults to logging.INFO.
+
+    Returns:
+        _logger_: logger object
+    """   
     logger = logging.getLogger(name)
     logger.setLevel(level)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
